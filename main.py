af2f278c0e0ebd0c8a9c92e2e4037fa7a78b57b6
Maha Gul <maha@example.com>
2026-08-05 10:00:13 +0100

    Create basic Input/Output Logic #7

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#menu and getting user input
+# menu and getting user input
+print(f"Welcome to the Quiz Application!")
+print(f"Enter S to Start the quiz or Q to Quit the application.")
+user_choice = input("Please select an option: ")
+
+while user_choice.lower() not in ["s", "q"]:
+    print(f"Invalid option selected. Please try again.")
+    user_choice = input("Please select an option: ")
+if user_choice.lower() == "s":
+    print(f"Quiz started...")
+    # Call the function to start the quiz
+    # start_quiz()
+elif user_choice.lower() == "q":
+    print(f"Quiz Closed...")
+
+    # Call the function to quit the application
+    # quit_application()
