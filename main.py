4e96fd5e633201bcc5c485c7a78aec659249c926
MadihaGul <engrmadihagul@gmail.com>
2026-08-05 18:54:12 +0200

    Load and print questions #5

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # menu and getting user input
+
+from data import load_questions
+
+
 print(f"Welcome to the Quiz Application!")
 print(f"Enter S to Start the quiz or Q to Quit the application.")
 user_choice = input("Please select an option: ")
@@ -7,11 +11,20 @@ while user_choice.lower() not in ["s", "q"]:
     print(f"Invalid option selected. Please try again.")
     user_choice = input("Please select an option: ")
 if user_choice.lower() == "s":
-    print(f"Quiz started...")
-    # Call the function to start the quiz
+    print("Quiz started...")
     # start_quiz()
+
+    title, questions = load_questions("questions.json")
+    print(f"Quiz Title: {title}")
+    n = 9
+    for question in questions:
+        print(f"Question {len(questions)-n}: {question['question']}")
+        print(f"Options: {question['options']}")
+        print(f"Answer: {question['correct_answer']}")
+        print(f"Explanation: {question['explanation']}")
+        n -= 1
 elif user_choice.lower() == "q":
-    print(f"Quiz Closed...")
+    print("Quiz Closed...")
 
     # Call the function to quit the application
     # quit_application()
